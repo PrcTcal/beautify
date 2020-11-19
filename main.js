04d0ed9a5c555f68f07773884315b4942be2d5bc
glen <hojjang0826@naver.com>
2020-11-19 22:53:29 +0900

    header finished

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+window.onload = function(){
+    document.querySelector('.menu-bar').style.height='0px';
+}
+
 function menudrop(){
-    
+    console.log('start');
+    var bar = document.querySelector('.menu-bar');
+    if(bar.style.height == '0px'){
+        console.log('늘리기')
+        bar.style.height = '140px';
+    } else {
+        console.log('줄이기');
+        bar.style.height = '0px';
+    }
 }
