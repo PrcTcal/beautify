adb2b7e45c8c0bcb0d583fea229a0521ff507e52
PrcTcal <hojjang0826@gmail.com>
2020-11-22 20:07:22 +0900

    The Clock - text glowing effect added

--- main.js
+++ main.js
@@ -9,7 +9,9 @@ window.onload = function(){
             let hour = date.getHours() < 10 ? '0' + date.getHours() : date.getHours();
             let minute = date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes();
             let second = date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds();
-            time.textContent = hour + ":" + minute + ":" + second;
+            let times = hour + ":" + minute + ":" + second;
+            time.setAttribute('data-text',times);
+            time.textContent = times;
         });
     });
 
