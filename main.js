b9b98836c1a4d767edd2f742dc85db4156b9eec2
PrcTcal <hojjang0826@gmail.com>
2020-11-22 15:55:03 +0900

    Clock practice

--- main.js
+++ main.js
@@ -1,15 +1,30 @@
 window.onload = function(){
     document.querySelector('.menu-bar').style.height='0px';
+    var clock = document.querySelector('.card2');
+    var interval;
+    clock.addEventListener('mouseover', function(){
+        interval = setInterval(() => {
+            const time = document.querySelector('.clock div h1');
+            let date = new Date();
+            let hour = date.getHours() < 10 ? '0' + date.getHours() : date.getHours();
+            let minute = date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes();
+            let second = date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds();
+            time.textContent = hour + ":" + minute + ":" + second;
+        });
+    });
+
+    clock.addEventListener('mouseout', function(){
+        clearInterval(interval);
+    })
 }
 
 function menudrop(){
-    console.log('start');
     var bar = document.querySelector('.menu-bar');
     if(bar.style.height == '0px'){
-        console.log('늘리기')
         bar.style.height = '140px';
     } else {
-        console.log('줄이기');
         bar.style.height = '0px';
     }
 }
+
+
